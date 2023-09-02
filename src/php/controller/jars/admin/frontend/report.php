051c2680fa75e90b9402932de225f7b22f28c431
Oran Fry <oran@emerg.co>
2023-09-02 15:15:57 +1200

    Groups
    Add support for the empty string group name.

--- src/php/controller/jars/admin/frontend/report.php
+++ src/php/controller/jars/admin/frontend/report.php
@@ -18,7 +18,7 @@ $path = [];
 
 foreach (explode('/', GROUP_NAME . (GROUP_NAME ? '/' : null) . 'fake') as $part) {
     $prefix = ($prefix = implode('/', $path)) ? $prefix . '/' : '';
-    $groups[] = $jars->groups(REPORT_NAME, $prefix, $min);
+    $groups[] = array_filter($jars->groups(REPORT_NAME, $prefix, $min));
     $path[] = $part;
 }
 
@@ -38,21 +38,19 @@ if (GROUP_NAME && 0 <= $index = count($groups) - 2) {
 
 $linetypes = $jars->linetypes(REPORT_NAME);
 
-if (!GROUP_NAME) {
-    $title = REPORT_NAME;
+$title = REPORT_NAME;
 
-    return compact('jars', 'fields', 'groups', 'path', 'linetypes', 'reports', 'title');
+if (GROUP_NAME) {
+    $title .= ' ' . GROUP_NAME;
 }
 
-$title = REPORT_NAME . ' ' . GROUP_NAME;
+$data = $jars->group(REPORT_NAME, GROUP_NAME, @$min);
 
 if ($jars->report(REPORT_NAME)->is_derived()) {
-    $data = $jars->group(REPORT_NAME, GROUP_NAME);
-
     return compact('jars', 'data', 'groups', 'path', 'reports', 'title');
 }
 
-$lines = $jars->group(REPORT_NAME, GROUP_NAME, @$min);
+$lines = &$data;
 $pos = array_search(GROUP_NAME, $groups);
 $prevGroup = $pos !== false && $pos > 0 ? $groups[$pos - 1] : null;
 $nextGroup = $pos !== false && $pos < count($groups) - 1 ? $groups[$pos + 1] : null;
