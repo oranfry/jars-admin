f9feac60ac138bf69e80c0b1cf3ed3b12266f514
Oran Fry <oran@emerg.co>
2023-11-18 15:17:14 +1300

    Floats
    Honour the decimal places property to set the step attribute in input HTML elements.

--- src/php/partial/fieldtype/float.php
+++ src/php/partial/fieldtype/float.php
@@ -1,1 +1,16 @@
-<input class="field value" type="number" name="<?= $name ?>" style="width: 8em" autocomplete="off">
+<?php
+
+(function() use ($name, $field) {
+    $step = 1 / pow(10, $field->dp ?? 0);
+
+    ?><input<?php
+
+    ?> autocomplete="off"<?php
+    ?> class="field value"<?php
+    ?> name="<?= $name ?>"<?php
+    ?> step="<?= $step ?>"<?php
+    ?> style="width: 8em"<?php
+    ?> type="number"<?php
+
+    ?>><?php
+})();
