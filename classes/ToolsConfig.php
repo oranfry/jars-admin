beeaef33d1b8303ad707342755819fe40cdf001e
Oran Fry <oran@emerg.co.nz>
2026-08-20 22:11:00 +1200

    Tools Config
    Conform to new method signature requirement for boot method, and use it to fetch and supply the version number for lines loading.

--- classes/ToolsConfig.php
+++ classes/ToolsConfig.php
@@ -2,8 +2,21 @@
 
 namespace OranFry\Jars\Admin;
 
+use OranFry\Jars\Contract\Client as JarsClient;
+
 class ToolsConfig extends \OranFry\Tools\Config
 {
+    public function boot(JarsClient $jars): array
+    {
+        $data = [];
+
+        if (isset($_GET['version']) && preg_match('/^[1-9][0-9]*$/', $_GET['version'])) {
+            $data['version'] = intval($_GET['version']);
+        }
+
+        return $data;
+    }
+
     public function custom(object $config, ?string $httpMountPoint, ?string $cliMountPoint, array $options): void
     {
         define('JARS_ADMIN_BASEPATH', $httpMountPoint);
